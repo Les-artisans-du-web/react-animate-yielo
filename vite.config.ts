eabe8fa8f8806371601d60570ec05a5f3db02196
Delmotte-Vincent <delmotte-vincent@outlook.fr>
2024-02-04 15:42:23 +0100

    chore: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "node:path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@animations": path.resolve(__dirname, "./src/animations"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "react-animate-yielo",
+    },
+    rollupOptions: {
+      external: ["react"],
+    },
+  },
+});

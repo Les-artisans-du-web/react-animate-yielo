4213be29363c19cac422970a09cbabe47999cc2b
Delmotte-Vincent <delmotte-vincent@outlook.fr>
2024-02-05 09:42:47 +0100

    chore: build config

--- src/index.ts
+++
@@ -1,1 +0,0 @@
-export { FadeInAndSlide } from "./animations";
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,31 @@
-import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@animations": path.resolve(__dirname, "./src/animations"),
-    },
-  },
+  plugins: [react(), dts({ include: "lib", insertTypesEntry: true })],
   build: {
+    // do not copy the contents of the public folder to the dist folder
+    copyPublicDir: false,
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
+      // this is the file that exports our components
+      entry: resolve(__dirname, "lib/react-animate-yielo.ts"),
       name: "react-animate-yielo",
+      fileName: "react-animate-yielo",
     },
     rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
       external: ["react"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
     },
   },
 });
